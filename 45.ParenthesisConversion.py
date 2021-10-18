33466029ab185361c48e44e7463abfeb8e7429fa
James <woghd8503@naver.com>
2021-10-18 23:45:22 +0900

    ParenthesisConversion

---
+++ 45.ParenthesisConversion.py
@@ -0,0 +1,24 @@
+# 이 코드는 다음 프로그래머스 사이트에서 테스트해야 정상 동작한다.
+# https://programmers.co.kr/learn/courses/30/lessons/60058
+
+# "균형잡힌 괄호 문자열"의 인덱스 반환
+def balanced_index(p):
+    count = 0 # 왼쪽 괄호의 개수
+    for i in range(len(p)):
+        if p[i] == '(':
+            count += 1
+        else:
+            if count == 0: # 쌍이 맞지 않는 경우에 False 반환
+                return False
+            count -= 1
+    return True # 쌍이 맞는 경우에 True 반환
+
+def solution(p):
+    answer = ''
+    if p == '':
+        return answer
+    index = balanced_index(p)
+    u = p[:index + 1]
+    v = p[index + 1:]
+    # "올바른 괄호 문자열"이 아니라면 아래의 과정을 수행
+    
