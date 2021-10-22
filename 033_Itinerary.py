f1a78a962d5a4a320987966bb11ac0d942ae6b96
James <woghd8503@naver.com>
2021-10-22 23:46:37 +0900

    ltinerary

---
+++ 033_Itinerary.py
@@ -0,0 +1,27 @@
+# a, b, c = map(int, input().split()) # 5 8 3
+# data = list(map(int, input().split())) # 2 4 5 4 6
+#
+# data.sort()
+# first = data[a - 1]
+# second = data[a - 2]
+#
+# result = 0
+#
+# while True:
+#     for i in range(c):
+#         if b == 0:
+#             break
+#         result += first
+#         b -= 1
+#     if b == 0:
+#         break
+#     result += second
+#     b -= 1
+#
+# print(result)
+#
+# '''
+# 5 8 3
+# 2 4 5 4 6
+# '''
+# 수정..............
