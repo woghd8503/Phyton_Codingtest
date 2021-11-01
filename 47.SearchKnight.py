1f433028904afa7d57e2f2c48442a125b5712d58
James <woghd8503@naver.com>
2021-11-01 23:45:39 +0900

    SearchKnight

--- 47.SearchKnight.py
+++ 47.SearchKnight.py
@@ -1,14 +1,19 @@
-# 집의 개수(N)와 공유기의 개수(C)를 입력받기
-n, c = list(map(int, input().split(' ')))
+# 이 코드는 다음 프로그래머스 사이트에서 테스트해야 정상 동장한다.
+# https://programmers.co.kr/learn/cources/30/lessons/60060
 
-# 전체 집의 좌표 정보를 입력 받기
-array = []
-for _ in range(n):
-    array.append(int(input()))
-array.sort() # 이진 탐색 수행을 위해 정력 수행
+from bisect import bisect_left, bisect_right
 
-start = array[1] - array[0] # 집의 좌표 중에 가장 작은 값
-end = array[-1] - array[0] # 집의 좌표 중에 가장 큰 값
-result = 0
+# 값이 [left_value, right_value]인 데이터의 개수를 반환하는 함수
+def count_by_range(a, left_value, right_value):
+    right_index = biscet_right(a, right_value)
+    left_index = bisect_left(a, left_value)
+    return right_index - left_index
 
-while
+# 모든 단어를 길이마다 나누어서 저장하기 위한 리스트
+array = [[] for _ in range(10001)]
+
+def solution(words, queries):
+    answer = []
+    for word in words: # 모든 단어를 접미사 와일드카드 배열, 접두사 와일드카드 배열에 각각 삽입
+        array[len(word)].append(word) # 단어들 삽입
+        reversed_array[]
