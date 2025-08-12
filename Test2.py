a490486a5c35657007de338a3438f9a7f4ec9066
James Park <66048976+woghd8503@users.noreply.github.com>
2025-08-12 23:29:06 +0900

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,54 +1,3 @@
-# n = int(input())
-# array = []
-#
-# for i in range(n):
-#     array.append(int(input()))
-#
-# array = sorted(array, reverse=True)
-#
-# for i in array:
-#     print(i, end=' ')
-
-
-# def squential_search(n, target, array):
-#     for i in range(n):
-#         if array[i] == target:
-#             return i + 1
-#
-# num = input().split()
-#
-# n = int(num[0])
-# target = num[1]
-#
-# array = input().split()
-#
-# print(squential_search(n, target, array))
-
-
-
-
-# def squential_search(n, target, array):
-#     for i in range(n):
-#         if array[i] == target:
-#             return i + 1
-#
-# info = input().split()
-# n = int(info[0])
-# target = info[1]
-# array = input().split()
-#
-# print(squential_search(n, target, array))
-
-# n = int(input())
-# array = []
-# for i in range(n):
-#     array.append(int(input()))
-#
-# array = sorted(array, reverse = True)
-#
-# for i in array:
-#     print(i, end=' ')
-
 def sequential_search(n, target, array):
     for i in range(n):
         if array[i] == target:
@@ -61,12 +10,3 @@ target = info[1]
 array = input().split()
 
 print(sequential_search(n, target, array))
-
-
-
-
-
-
-
-
-
