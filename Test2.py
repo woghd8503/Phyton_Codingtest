b36ae25b0ffcc82623f31f1dffb779a0774c2d9c
James Park <66048976+woghd8503@users.noreply.github.com>
2025-08-14 23:58:10 +0900

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,12 +1,1 @@
-def sequential_search(n, target, array):
-    for i in range(n):
-        if array[i] == target:
-            return i + 1
 
-info = input().split()
-n = int(info[0])
-target = info[1]
-
-array = input().split()
-
-print(sequential_search(n, target, array))
